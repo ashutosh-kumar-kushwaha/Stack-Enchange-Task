f7713f3efba320155ed08eccd40d8f81a49bb16c
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-05-10 01:09:08 +0530

    Add Fragments for different types of questions

---
+++ app/src/main/java/ashutosh/stackExchangeTask/ui/home/HotQuestionsFragment.kt
@@ -0,0 +1,18 @@
+package ashutosh.stackExchangeTask.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ashutosh.stackExchangeTask.R
+
+class HotQuestionsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hot_questions, container, false)
+    }
+}
